a157c897eff4bf736aa94e78967be6a67db3b6fe
onochihiro0822 <ono@ibis-japan.co.jp>
2026-05-25 11:48:53 +0900

    Issue6

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -66,9 +66,13 @@ public class CalendarsController {
           }
       }
 
+      // 曜日番号を計算	day_map.put("month", currentDate.getMonthValue());
+      int wdayNum = (todaysDate.getDayOfWeek().getValue() + x) % 7;
+
       day_map.put("month", currentDate.getMonthValue());
       day_map.put("date", currentDate.getDayOfMonth());
       day_map.put("plans", todayPlans);
+      day_map.put("wday", wdays[wdayNum]);
 
       weekDays.add(day_map);
     }
