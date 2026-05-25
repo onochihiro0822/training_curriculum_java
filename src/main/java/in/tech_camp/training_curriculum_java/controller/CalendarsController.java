dff9dfcf923101ce7e612ed2c7b3c34784f180d2
onochihiro0822 <ono@ibis-japan.co.jp>
2026-05-25 12:01:01 +0900

    dayMap

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -56,7 +56,7 @@ public class CalendarsController {
     String[] wdays = {"(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"};
 
     for (int x = 0; x < 7; x++) {
-      Map<String, Object> day_map = new HashMap<String, Object>();
+      Map<String, Object> dayMap = new HashMap<String, Object>();
       LocalDate currentDate = todaysDate.plusDays(x);
 
       List<String> todayPlans = new ArrayList<>();
@@ -69,12 +69,12 @@ public class CalendarsController {
       // 曜日番号を計算	day_map.put("month", currentDate.getMonthValue());
       int wdayNum = (todaysDate.getDayOfWeek().getValue() + x) % 7;
 
-      day_map.put("month", currentDate.getMonthValue());
-      day_map.put("date", currentDate.getDayOfMonth());
-      day_map.put("plans", todayPlans);
-      day_map.put("wday", wdays[wdayNum]);
+      dayMap.put("month", currentDate.getMonthValue());
+      dayMap.put("date", currentDate.getDayOfMonth());
+      dayMap.put("plans", todayPlans);
+      dayMap.put("wday", wdays[wdayNum]);
 
-      weekDays.add(day_map);
+      weekDays.add(dayMap);
     }
 
     return weekDays;
